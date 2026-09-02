d89bc166116f7f966daff59854580ebd80cf5481
leaptt168-rgb <leaptt168@gmail.com>
2026-09-02 18:51:28 +0700

    dsd

--- obj/Debug/net10.0/SchoolApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/SchoolApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3468781a3290faacf8ddda0e681c63c172e1ad2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30c2ae17ec3afbededd8e6fd6716a9634c67624f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
